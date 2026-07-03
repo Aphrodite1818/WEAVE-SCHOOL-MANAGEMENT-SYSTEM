d67642cf55e054bffd0cdd919c1df4d00a7743cb
Taiwo <taiwoayimora623@gmail.com>
2026-07-03 15:02:10 +0100

    testing redis

--- backend/app/core/cache/redis.py
+++ backend/app/core/cache/redis.py
@@ -41,8 +41,8 @@ async def connect_redis() -> None:
         settings.REDIS_URL,
         encoding = "utf-8",
         decode_responses = True,
-        socket_timeout = 5,
-        socket_connect_timeout = 5,
+        socket_timeout = 0.5,
+        socket_connect_timeout = 0.5,
         health_check_interval = 30,
     )
 
