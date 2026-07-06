7d9c8f6771c26b2ec1205a909a0ddc1f96b9d007
Aphrodite1818 <taiwoayimora623@gmail.com>
2026-07-06 14:18:44 +0100

    Split public routes

---
+++ frontend/src/routes/publicRoutes.jsx
@@ -0,0 +1,19 @@
+import { Route } from "react-router-dom";
+
+import ForgotPasswordPage from "../pages/public/ForgotPasswordPage";
+import InvitePage from "../pages/public/InvitePage";
+import LandingPage from "../pages/public/LandingPage";
+import LoginPage from "../pages/public/LoginPage";
+import OTPValidationPage from "../pages/public/otp_validationPage";
+import RegisterPage from "../pages/public/RegisterPage";
+
+export const publicRoutes = (
+  <>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/verify-otp" element={<OTPValidationPage />} />
+    <Route path="/invite" element={<InvitePage />} />
+    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+  </>
+);
