d0af336f092ed1b99c1a4a0f298ae105c3b04f72
Aphrodite1818 <taiwoayimora623@gmail.com>
2026-07-06 09:12:18 +0100

    Load mobile override styles last

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/mobileDashboard.css'
+import './styles/mobileOverrides.css'
 import App from './App.jsx'
 
 const savedTheme = localStorage.getItem("theme");
