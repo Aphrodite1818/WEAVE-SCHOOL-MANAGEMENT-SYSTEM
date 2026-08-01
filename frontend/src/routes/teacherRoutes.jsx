c33a04908d7da67390fe41bd44942fd629d4074b
Aphrodite1818 <taiwoayimora623@gmail.com>
2026-08-01 01:58:46 +0100

    feat: route teacher getting started page

--- frontend/src/routes/teacherRoutes.jsx
+++ frontend/src/routes/teacherRoutes.jsx
@@ -1,18 +1,19 @@
 import { Route } from "react-router-dom";
 
 import { DashboardShell } from "../components/layout/DashboardLayout";
+import CommunicationInboxPage from "../pages/shared/CommunicationInboxPage";
+import MessagesPage from "../pages/shared/MessagesPage";
+import RoleAnalyticsPage from "../pages/shared/RoleAnalyticsPage";
+import RoleGettingStartedPage from "../pages/shared/RoleGettingStartedPage";
+import RoleSettingsPage from "../pages/shared/RoleSettingsPage";
+import SchoolCalendarPage from "../pages/shared/SchoolCalendarPage";
+import SchoolSwitchPage from "../pages/shared/SchoolSwitchPage";
 import TeacherAttendancePage from "../pages/teacher/AttendancePage";
-import TeacherDashboardPage from "../pages/teacher/TeacherDashboardPage";
 import TeacherClassesPage from "../pages/teacher/MyClassesPage";
 import TeacherResultsPage from "../pages/teacher/ResultsPage";
 import TeacherStudentsPage from "../pages/teacher/StudentsPage";
 import TeacherSubjectsPage from "../pages/teacher/SubjectsPage";
-import CommunicationInboxPage from "../pages/shared/CommunicationInboxPage";
-import MessagesPage from "../pages/shared/MessagesPage";
-import RoleAnalyticsPage from "../pages/shared/RoleAnalyticsPage";
-import SchoolCalendarPage from "../pages/shared/SchoolCalendarPage";
-import RoleSettingsPage from "../pages/shared/RoleSettingsPage";
-import SchoolSwitchPage from "../pages/shared/SchoolSwitchPage";
+import TeacherDashboardPage from "../pages/teacher/TeacherDashboardPage";
 import MembershipScopeGuard from "./MembershipScopeGuard";
 import RoleGuard from "./RoleGuard";
 import RuntimeFeatureRoute from "./RuntimeFeatureRoute";
@@ -26,6 +27,7 @@ export const teacherRoutes = (
     <Route element={<MembershipScopeGuard role="teacher" />}>
       <Route element={<DashboardShell role="teacher" />}>
         <Route path="/teacher/dashboard" element={<TeacherDashboardPage />} />
+        <Route path="/teacher/getting-started" element={<RoleGettingStartedPage role="teacher" />} />
         <Route path="/teacher/analytics" element={<RoleAnalyticsPage role="teacher" />} />
         <Route path="/teacher/classes" element={<TeacherClassesPage />} />
         <Route path="/teacher/students" element={<TeacherStudentsPage />} />
