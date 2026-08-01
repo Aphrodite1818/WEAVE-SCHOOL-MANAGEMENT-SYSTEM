5ae69d24c11f1a2156a776cc066727b1c2a92ee2
Aphrodite1818 <taiwoayimora623@gmail.com>
2026-08-01 01:59:10 +0100

    feat: route parent getting started page

--- frontend/src/routes/parentRoutes.jsx
+++ frontend/src/routes/parentRoutes.jsx
@@ -1,15 +1,16 @@
 import { Route } from "react-router-dom";
 
 import { DashboardShell } from "../components/layout/DashboardLayout";
+import ParentAttendancePage from "../pages/parent/ParentAttendancePage";
 import ParentDashboardPage from "../pages/parent/ParentDashboardPage";
+import ParentReportCardsPage from "../pages/parent/ParentReportCardsPage";
 import ParentResultsPage from "../pages/parent/ParentResultsPage";
 import ParentStudentLinkingPage from "../pages/parent/ParentStudentLinkingPage";
-import ParentReportCardsPage from "../pages/parent/ParentReportCardsPage";
-import ParentAttendancePage from "../pages/parent/ParentAttendancePage";
 import CommunicationInboxPage from "../pages/shared/CommunicationInboxPage";
 import MessagesPage from "../pages/shared/MessagesPage";
-import SchoolCalendarPage from "../pages/shared/SchoolCalendarPage";
+import RoleGettingStartedPage from "../pages/shared/RoleGettingStartedPage";
 import RoleSettingsPage from "../pages/shared/RoleSettingsPage";
+import SchoolCalendarPage from "../pages/shared/SchoolCalendarPage";
 import SchoolSwitchPage from "../pages/shared/SchoolSwitchPage";
 import MembershipScopeGuard from "./MembershipScopeGuard";
 import RoleGuard from "./RoleGuard";
@@ -24,6 +25,7 @@ export const parentRoutes = (
     <Route element={<MembershipScopeGuard role="parent" />}>
       <Route element={<DashboardShell role="parent" />}>
         <Route path="/parent/dashboard" element={<ParentDashboardPage />} />
+        <Route path="/parent/getting-started" element={<RoleGettingStartedPage role="parent" />} />
         <Route path="/parent/student-linking" element={<ParentStudentLinkingPage />} />
         <Route path="/parent/report-cards" element={<ParentReportCardsPage />} />
         <Route path="/parent/attendance" element={<RuntimeFeatureRoute feature="attendance" role="parent"><ParentAttendancePage /></RuntimeFeatureRoute>} />
